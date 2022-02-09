0143e4422aba2e2016f2eef58458678e404da962
KChesh <981>
2022-02-09 10:56:22 +0300

    Добавление пробелов

--- web/resources/js/library.js
+++ web/resources/js/library.js
@@ -15,6 +15,6 @@ function showProgress(data) {
     if (data.status == "begin") {
         document.getElementById('loading_wrapper').style.display = "block";
     } else if (data.status == "success") {
-        document.getElementById('loading_wrapper').style.display = "none";
+           document.getElementById('loading_wrapper').style.display = "none";
     }
 }
